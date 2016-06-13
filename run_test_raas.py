4902614af29db0105f4efce4717a39cfcd8338fd
Liyou Zhou <liyou.zhou@arm.com>
2016-06-13 16:41:17 +0100

    Always disconnect the client to kill background threads

--- run_test_raas.py
+++ run_test_raas.py
@@ -6,32 +6,34 @@ def run_test_raas(binary, platform_name="K64F"):
 
     client = raas_client.RaasClient(host="193.208.80.31", port=8007)
 
-    k64f = client.allocate({"platform_name": [platform_name]})
-    print "allocated", k64f.info()
+    try:
+        k64f = client.allocate({"platform_name": [platform_name]})
+        print "allocated", k64f.info()
 
-    print "flashing...",
-    k64f.flash(binary)
-    print "flashed"
+        print "flashing...",
+        k64f.flash(binary)
+        print "flashed"
 
-    serial_params = raas_client.SerialParameters(lineMode=True, baudrate=9600, bufferSize=256)
-    print "opening connection to k64f"
-    k64f.openConnection(parameters=serial_params)
+        serial_params = raas_client.SerialParameters(lineMode=True, baudrate=9600, bufferSize=256)
+        print "opening connection to k64f"
+        k64f.openConnection(parameters=serial_params)
 
-    print "resetting...",
-    k64f.reset()
-    print "reset"
+        print "resetting...",
+        k64f.reset()
+        print "reset"
 
-    data = "start"
-    print "starting serial capture"
-    serial_out = ""
-    while(data != ""):
-        data = k64f.readline(timeout=10)
-        serial_out += data
-        print "[DATA]", data.strip()
+        data = "start"
+        print "starting serial capture"
+        serial_out = ""
+        while(data != ""):
+            data = k64f.readline(timeout=10)
+            serial_out += data
+            print "[DATA]", data.strip()
 
-    k64f.closeConnection()
-    k64f.release()
-    client.disconnect()
+        k64f.closeConnection()
+        k64f.release()
+    finally:
+        client.disconnect()
 
     return serial_out
 
